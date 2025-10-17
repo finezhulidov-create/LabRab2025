abb58a4b6a514a342853fcec19a840f89b833cd2
ZhulidovAl <fine.zhulidov@gmail.com>
2025-10-18 03:29:44 +0500

    12lr

--- src/lab01/Example12.java
+++ src/lab01/Example12.java
@@ -1,4 +1,18 @@
 package lab01;
+import java.util.Scanner;
+
+//Напишите программу, в которой по возрасту определяется год рождения.
 
 public class Example12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите возраст");
+        int num = in.nextInt();
+        int    num1 = 2025-num;
+
+        System.out.println(num1);
+        in.close();
+    }
+
 }
