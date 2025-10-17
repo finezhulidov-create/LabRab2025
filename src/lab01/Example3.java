8b442c48721524c6b24873a4665e6c98e0bf8ff0
Daiwaa1 <zhulidov2013@gmail.com>
2025-10-18 01:51:55 +0500

    1

--- src/lab01/Example3.java
+++ src/lab01/Example3.java
@@ -1,4 +1,22 @@
 package lab01;
 
 public class Example3 {
+    public static void main(String[] args) {
+        int s, d;
+        s = 10;
+        d = 20;
+        if (s < d) {
+            System.out.println("S < D");
+        }
+        s = s * 2;
+        if (s == d) {
+            System.out.println("S = D");
+        }
+        s = s * d;
+        if (s > d) {
+            System.out.println("S > D");
+        }
+
+    }
+
 }
--- src/lab01/Example4.java
+++ src/lab01/Example4.java
@@ -1,4 +1,12 @@
 package lab01;
-
+import java.util.Scanner;
 public class Example4 {
+            System.out.println("Input a number:");
+    Scanner in = new Scanner(System.in);
+    int num = in.nextInt();
+
+        System.out.println("Your number" + num);
+        in.close();
+}
+
 }
