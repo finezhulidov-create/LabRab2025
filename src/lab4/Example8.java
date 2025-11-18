0a1aeb1f6feb48fb4e40df68d28cbc6fb4b969ef
ZhulidovAl <fine.zhulidov@gmail.com>
2025-11-18 16:58:43 +0500

    init commit

--- src/lab4/Example8.java
+++ src/lab4/Example8.java
@@ -9,7 +9,26 @@ public class Example8 {
         String value = in.nextLine();
         System.out.println("Введите ключ");
         int key = in.nextInt();
-
+        String encriptedText = encrypt(value, key);
+        System.out.println("Текст после преобразования: \n" + encriptedText);
+        in.nextLine();
+        while(true) {
+            System.out.println("Выполнить обратное преобразование? (y/n)");
+            String answer = in.nextLine();
+            if (answer.equalsIgnoreCase("y")){
+                String decryptedText = decrypt(encriptedText, key);
+                System.out.println("Обратно преобразованный текст: " + decryptedText);
+                break;
+            } else if (answer.equalsIgnoreCase("n")) {
+                System.out.println("До свидания!");
+                break;
+            } else {
+                System.out.println("Введите корректный ответ");
+            }
+        }
+        in.close();
+    }
+    public static String encrypt(String value, int key){
         char[] chars = value.toCharArray();
         int[] ints = new int[value.length()];
         for (int i = 0; i < chars.length; i++) {
@@ -17,29 +36,18 @@ public class Example8 {
             chars[i] = (char) ints[i];
         }
         String str = new String(chars);
-        System.out.println("Текст после преобразования: \n" + str);
-        in.nextLine();
-        System.out.println("Выполнить обратное преобразование? (y/n)");
-        String yOrN = in.nextLine();
-
-
-        char[] chars1 = str.toCharArray();
-        int[] ints1 = new int[str.length()];
+        return str;
+    }
+    public static String decrypt(String encriptedText, int key){
+        char[] chars1 = encriptedText.toCharArray();
+        int[] ints1 = new int[encriptedText.length()];
         for (int i = 0; i < chars1.length; i++) {
             ints1[i] = chars1[i] - key;
             chars1[i] = (char) ints1[i];
         }
-            String str1 = new String(chars1);
+        String str1 = new String(chars1);
 
-        if (yOrN.equalsIgnoreCase("y")) {
-            System.out.println("Текст после преобразования: \n" + str1);
-        }
-        if (yOrN.equalsIgnoreCase("n")) {
-            System.out.println("До свидания!");
-        } else {
-            System.out.println("Введите корректный ответ");
-
-        }
+        return str1;
         }
     }
 
