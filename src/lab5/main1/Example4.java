fdd2fd49242cf346ae7c702c664f52c7ac9f0898
ZhulidovAl <fine.zhulidov@gmail.com>
2025-11-20 04:50:43 +0500

    init commit

---
+++ src/lab5/main1/Example4.java
@@ -0,0 +1,28 @@
+package lab5.main1;
+//Напишите программу с классом, у которого есть символьное и целочисленное поле.
+// В классе должны быть описаны версии конструктора с двумя аргументами (целое число и символ – определяют значения полей),
+// а также с одним аргументом типа double. В последнем случае действительная часть аргумента определяет код символа (значение символьного поля),
+// а дробная часть (с учетом десятых и сотых) определяет значение целочисленного поля. Например, если аргументом передается число 65.1267,
+// то значением символьного поля будет символ ‘A’ с годом 65, а целочисленное поле получит значение 12 (в дробной части учитываются только десятые и сотые).
+public class Example4 {
+    private char oneChar;
+    private int oneInt;
+
+    public Example4(char oneChar, int oneInt) {
+        this.oneChar = oneChar;
+        this.oneInt = oneInt;
+    }
+
+    public Example4(double doubleValue) {
+        this.oneInt = (int) (doubleValue - this.oneInt);;
+        this.oneChar = (char) doubleValue;
+    }
+
+    public char getOneChar() {
+        return oneChar;
+    }
+
+    public int getOneInt() {
+        return oneInt;
+    }
+}
---
+++ src/lab5/main1/Main4.java
@@ -0,0 +1,10 @@
+package lab5.main1;
+
+public class Main4 {
+    public static void main(String[] args) {
+        Example4 e = new Example4('a',100);
+        System.out.println(e.getOneChar()+" "+ e.getOneInt());
+        Example4 e1 = new Example4(68.3456);
+        System.out.println(e1.getOneInt()+ " "+ e1.getOneChar());
+    }
+}
